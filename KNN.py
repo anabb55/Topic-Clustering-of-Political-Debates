f6c68473d4266398d4d0ce0ec294fc9838528d57
anabb55 <75089113+anabb55@users.noreply.github.com>
2025-11-12 00:03:18 +0100

    scaled data

--- KNN.py
+++ KNN.py
@@ -28,7 +28,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.fit_transform(X_test)
+X_test_scaled = scaler.transform(X_test)
 
 model = KNeighborsClassifier()
 
@@ -44,10 +44,10 @@ tuner = GridSearchCV(
     cv=5
 )
 
-tuner.fit(X_train, y_train)
+tuner.fit(X_train_scaled, y_train)
 
 best_model = tuner.best_estimator_
-y_pred = best_model.predict(X_test)
+y_pred = best_model.predict(X_test_scaled)
 print(f"Best combination of hyperparameters:  {tuner.best_params_}")
 
 
